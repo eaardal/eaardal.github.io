d44816c18ce96d986890d5040b4e59dd66ce4d29
Eirik Årdal <eaardal@outlook.com>
2014-11-29 11:36:47 +0100

    Squares changes color upon collision

--- nerdschool-ex3-impl/main.js
+++ nerdschool-ex3-impl/main.js
@@ -12,16 +12,21 @@ var Game = function() {
 
 	this.redraw = function(){
 		if (this.context === undefined ) {
-			console.log('no context, returning...')
+			printToConsole('no context, returning...')
 			return;
 		}
 
 		this.context.clearRect(0, 0, 400, 400);
 		var context = this.context;
 		var canvas = this.canvas;
+		var squares = this.squares;
 		this.squares.forEach(function(square){
 			square.computeSquareMove(canvas);
+			if (square.isColliding(squares)){
+				square.switchColor();
+			}
 			context.beginPath();
+			context.strokeStyle = square.color;
 			context.rect(square.x, square.y, square.width, square.height);
 			context.stroke();
 			context.closePath();
@@ -31,7 +36,7 @@ var Game = function() {
 
 var gameLoop = function(){
 	if (game.squares === undefined) {
-		console.log('no squares, returning...')
+		printToConsole('no squares, returning...')
 		return;
 	}
 	window.requestAnimationFrame(gameLoop);
@@ -39,19 +44,21 @@ var gameLoop = function(){
 }
 
 var Square = function(){
-	this.width = 15;
-	this.height = 15;
+	var colors = ['blue', 'red'];
+	this.width = 30;
+	this.height = 30;
 	this.x = -1;
 	this.y = -1;
-	this.velocityX = 3;
-	this.velocityY = 3;
+	this.velocityX = 1;
+	this.velocityY = 1;
+	this.color = colors[0];
 
 	this.computeSquareMove = function(canvas){
 
 		this.x += this.velocityX;
 		this.y += this.velocityY;
 
-		if (totalWidth() >= canvas.width){			
+		if (this.totalWidth() >= canvas.width){			
 			this.velocityX = invertNumber(this.velocityX);
 		} 
 
@@ -59,7 +66,7 @@ var Square = function(){
 			this.velocityX = invertNumber(this.velocityX);
 		}
 
-		if (totalHeight() >= canvas.height){
+		if (this.totalHeight() >= canvas.height){
 			this.velocityY = invertNumber(this.velocityY);
 		} 
 
@@ -68,13 +75,49 @@ var Square = function(){
 		}
 	}
 
+	this.isColliding = function(squares){
+		if (squares === undefined){
+			return;
+		}
+
+		for (var i = 0; i < squares.length; i++) {
+			var square = squares[i];
+			if (square === this){
+				continue;
+			}
+
+			if (this.x < square.x + square.width  && 
+				this.x + this.width  > square.x &&
+				this.y < square.y + square.height && 
+				this.y + this.height > square.y) {
+				return true;
+			}
+		};
+		return false;
+	}
+
+	this.switchColor = function(){
+		/* 
+
+		Toggles between random colors in colors array:
+
+		var color = this.color;
+		while (color === this.color){
+			var index = getRandomNumber(0, colors.length);
+			color = colors[index];	
+		}		
+		this.color = color;
+		*/
+		this.color = colors[1];
+	}
+
 	var square = this;
 
-	var totalWidth = function(){
+	this.totalWidth = function(){
 		return square.x + square.width;
 	}
 
-	var totalHeight = function(){
+	this.totalHeight = function(){
 		return square.y + square.height;
 	}
 }
--- nerdschool-ex3-impl/utils.js
+++ nerdschool-ex3-impl/utils.js
@@ -16,3 +16,8 @@ var invertNumber = function(number){
 		return Math.abs(number);
 	}
 }
+
+var getRandomNumber = function(min, max){
+	// Thx to http://stackoverflow.com/questions/4959975/generate-random-value-between-two-numbers-in-javascript
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
