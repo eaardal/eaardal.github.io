d5be65e71ea47d648abccdfe1cdaf3f6ccb35210
Eirik Årdal <eiard@statoil.com>
2014-12-01 16:33:40 +0100

    bug

--- nerdschool-ex3-impl/main.js
+++ nerdschool-ex3-impl/main.js
@@ -1,20 +1,36 @@
 var Game = function() {
-	this.canvas = null;
-	this.context = null;
-	this.squares = [];
+    this.canvas = null;
+    this.context = null;
+    this.squares = [];
 
-	this.handleSquare = function(event){
-		if (!this.removeSquare(event)){
-			this.makeSquare(event);
-		}
-	}
+    this.handleSquare = function(event) {
+        if (!this.removeSquare(event)) {
+            this.makeSquare(event);
+        }
+    }
 
-	this.makeSquare = function(event){
-		var square = new Square();
-		square.x = event.layerX;
-		square.y = event.layerY;
-		this.squares.push(square);
-	}
+    this.makeSquare = function(event) {
+        var square = new Square();
+
+        square.x = event.layerX;
+        square.y = event.layerY;
+
+        var ensureIsCreatedWithinBounds = function() {
+            var buffer = 5;
+            
+            if (square.totalWidth() + buffer > this.canvas.width) {
+                square.x = this.canvas.width - (square.width + buffer);
+            }
+
+            if (square.totalHeight() + buffer > this.canvas.height) {
+                square.y = this.canvas.height - (square.height + buffer);
+            }
+        }
+
+        ensureIsCreatedWithinBounds();
+
+        this.squares.push(square);
+    }
 
     this.removeSquare = function(event) {
         var x = event.layerX;
@@ -38,102 +54,102 @@ var Game = function() {
         return removedSquare;
     }
 
-	this.redraw = function(){
-		if (this.context === undefined ) {
-			printToConsole('no context, returning...')
-			return;
-		}
+    this.redraw = function() {
+        if (this.context === undefined) {
+            printToConsole('no context, returning...')
+            return;
+        }
 
-		this.context.clearRect(0, 0, 400, 400);
-		var context = this.context;
-		var canvas = this.canvas;
-		var squares = this.squares;
-		this.squares.forEach(function(square){				
-			square.computeSquareMove(canvas);
-			if (square.isColliding(squares)){
-				square.switchColor();
-			}			
-			context.beginPath();
-			context.fillStyle = square.color;
-			context.fillRect(square.x, square.y, square.width, square.height);
-			context.stroke();
-			context.closePath();
-		});
-	}
+        this.context.clearRect(0, 0, 400, 400);
+        var context = this.context;
+        var canvas = this.canvas;
+        var squares = this.squares;
+        this.squares.forEach(function(square) {
+            square.computeSquareMove(canvas);
+            if (square.isColliding(squares)) {
+                square.switchColor();
+            }
+            context.beginPath();
+            context.fillStyle = square.color;
+            context.fillRect(square.x, square.y, square.width, square.height);
+            context.stroke();
+            context.closePath();
+        });
+    }
 }
 
-var gameLoop = function(){
-	if (game.squares === undefined) {
-		printToConsole('no squares, returning...')
-		return;
-	}
-	window.requestAnimationFrame(gameLoop);
-	game.redraw();
+var gameLoop = function() {
+    if (game.squares === undefined) {
+        printToConsole('no squares, returning...')
+        return;
+    }
+    window.requestAnimationFrame(gameLoop);
+    game.redraw();
 }
 
-var Square = function(){
-	var colors = ['blue', 'red'];
-	this.width = 40;
-	this.height = 40;
-	this.x = -1;
-	this.y = -1;
-	this.velocityX = getRandomNumber(1, 3);
-	this.velocityY = getRandomNumber(1, 3);
-	this.color = colors[0];
+var Square = function() {
+    var colors = ['blue', 'red'];
+    this.width = 40;
+    this.height = 40;
+    this.x = -1;
+    this.y = -1;
+    this.velocityX = getRandomNumber(1, 3);
+    this.velocityY = getRandomNumber(1, 3);
+    this.color = colors[0];
 
-	this.isClicked = function(x, y){
-		if (this.x < x && this.totalWidth() > x &&
-			this.y < y && this.totalHeight() > y) {
-			return true;
-		}
-		return false;
-	}
+    this.isClicked = function(x, y) {
+        if (this.x < x && this.totalWidth() > x &&
+            this.y < y && this.totalHeight() > y) {
+            return true;
+        }
+        return false;
+    }
 
-	this.computeSquareMove = function(canvas){
+    this.computeSquareMove = function(canvas) {
 
-		this.x += this.velocityX;
-		this.y += this.velocityY;
+        this.x += this.velocityX;
+        this.y += this.velocityY;
 
-		if (this.totalWidth() >= canvas.width){			
-			this.velocityX = invertNumber(this.velocityX);
-		} 
+        if (this.totalWidth() >= canvas.width) {
+            this.velocityX = invertNumber(this.velocityX);
+        }
 
-		if (this.x <= 0){
-			this.velocityX = invertNumber(this.velocityX);
-		}
+        if (this.x <= 0) {
+            this.velocityX = invertNumber(this.velocityX);
+        }
 
-		if (this.totalHeight() >= canvas.height){
-			this.velocityY = invertNumber(this.velocityY);
-		} 
+        if (this.totalHeight() >= canvas.height) {
+            this.velocityY = invertNumber(this.velocityY);
+        }
 
-		if (this.y <= 0){
-			this.velocityY = invertNumber(this.velocityY);
-		}
-	}
+        if (this.y <= 0) {
+            this.velocityY = invertNumber(this.velocityY);
+        }
+    }
 
-	this.isColliding = function(squares){
-		if (squares === undefined){
-			return;
-		}
+    this.isColliding = function(squares) {
+        if (squares === undefined) {
+            return;
+        }
 
-		for (var i = 0; i < squares.length; i++) {
-			var square = squares[i];
-			if (square === this){
-				continue;
-			}
+        for (var i = 0; i < squares.length; i++) {
+            var square = squares[i];
+            if (square === this) {
+                continue;
+            }
 
-			if (this.x < square.x + square.width  && 
-				this.x + this.width  > square.x &&
-				this.y < square.y + square.height && 
-				this.y + this.height > square.y) {
-				return true;
-			}
-		};
-		return false;
-	}
+            if (this.x < square.x + square.width &&
+                this.x + this.width > square.x &&
+                this.y < square.y + square.height &&
+                this.y + this.height > square.y) {
+                return true;
+            }
+        };
+        return false;
+    }
 
-	this.switchColor = function(){
-		/* 
+    this.switchColor = function() {
+        /* 
 
 		Toggles between random colors in colors array:
 
@@ -144,28 +160,28 @@ var Square = function(){
 		}		
 		this.color = color;
 		*/
-		this.color = colors[1];
-	}
+        this.color = colors[1];
+    }
 
-	this.totalWidth = function(){
-		return this.x + this.width;
-	}
+    this.totalWidth = function() {
+        return this.x + this.width;
+    }
 
-	this.totalHeight = function(){
-		return this.y + this.height;
-	}
+    this.totalHeight = function() {
+        return this.y + this.height;
+    }
 }
 
 var game = new Game();
 
-drawGameBoard = function(){
-	game.canvas = document.getElementById("gameBoard");
-	game.canvas.height = 400;
-	game.canvas.width = 400;
-	game.canvas.addEventListener('click', function(event){
-		game.handleSquare(event);
-	}, false);	
-	game.context = game.canvas.getContext("2d");
+drawGameBoard = function() {
+    game.canvas = document.getElementById("gameBoard");
+    game.canvas.height = 400;
+    game.canvas.width = 400;
+    game.canvas.addEventListener('click', function(event) {
+        game.handleSquare(event);
+    }, false);
+    game.context = game.canvas.getContext("2d");
 }
 
 gameLoop();
