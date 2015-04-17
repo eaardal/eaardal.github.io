edfba47fb833eadb6a6d4bc245ed992ae2ccdc67
Eirik Årdal <eaardal@outlook.com>
2015-04-17 21:13:13 +0200

    Added GET test

--- nerdschool-ci/app/ciapp/src/main/java/no/eaardal/MyResource.java
+++ nerdschool-ci/app/ciapp/src/main/java/no/eaardal/MyResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -21,5 +22,12 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it!";
+    }
+
+    @GET
+    @Path("/test")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response test(){
+        return Response.ok("hello world").build();
     }
 }
